fdd1b6688cb4a2ce671842f0a59926d0dceca95d
Klyuchevenko Ekaterina <ktrin1996@mail.ru>
2018-03-19 12:36:31 +0300

    add third task

---
+++ StorageInfoAboutAmountOfObjects/StorageInfoAboutAmountOfObjects/CounterOfObjects.cs
@@ -0,0 +1,12 @@
+﻿namespace StorageInfoAboutAmountOfObjects
+{
+  public class CounterOfObjects
+  {
+    public static int Counter { get; private set; }
+
+    public CounterOfObjects()
+    {
+      Counter++;     
+    }
+  }
+}
---
+++ StorageInfoAboutAmountOfObjects/StorageInfoAboutAmountOfObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StorageInfoAboutAmountOfObjects
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      CounterOfObjects counterOfObjects1 = new CounterOfObjects();
+      CounterOfObjects counterOfObjects2 = new CounterOfObjects();
+      CounterOfObjects counterOfObjects3 = new CounterOfObjects();
+
+      Console.WriteLine(CounterOfObjects.Counter.ToString());
+
+      Console.ReadKey();
+    }
+  }
+}
