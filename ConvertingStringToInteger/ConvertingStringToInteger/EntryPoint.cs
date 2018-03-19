4f0bf5790946a7669b4514b7779259e85bf4f5b7
Klyuchevenko Ekaterina <ktrin1996@mail.ru>
2018-03-19 10:08:59 +0300

    add task: converting string to integer

---
+++ ConvertingStringToInteger/ConvertingStringToInteger/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertingStringToInteger
+{
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter the string: ");
+      try
+      {
+        string inputString = Console.ReadLine();
+        Console.Write(inputString.ConvertStringToInteger());
+
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("There in no numbers in input string.");
+      }
+      Console.ReadKey();
+    }
+  }
+}
---
+++ ConvertingStringToInteger/ConvertingStringToInteger/StringExtensionMethods.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+
+namespace ConvertingStringToInteger
+{
+  public static class StringExtensionMethods
+  {
+    public static int ConvertStringToInteger(this String inputString)
+    {
+      if (!IsStringConsistsOnlyOfLetters(inputString))
+      {
+        throw new Exception();
+      }
+      //int index = IsStringHasNegativeNumber(inputString); 
+      //int result = Ibh(inputString, index); 
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int i = 0; i < inputString.Length; i++)
+      {
+        if (char.IsDigit(inputString[i]))
+        {
+          stringBuilder.Append(inputString[i]);
+          if (inputString[i] == inputString[inputString.Length - 1] || char.IsDigit(inputString[i + 1]))
+          {
+            continue;
+          }
+          else break;
+        }
+
+      }
+      int result = int.Parse(stringBuilder.ToString());
+      return result;
+    }
+
+    private static bool IsStringConsistsOnlyOfLetters(string inputString)
+    {
+      char[] numbers = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+      foreach (var number in numbers)
+      {
+        foreach (var element in inputString)
+        {
+          if (inputString.Contains(number.ToString()))
+          {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    //private static int IsStringHasNegativeNumber(string inputString) 
+    //{ 
+    // StringBuilder stringBuilder = new StringBuilder(); 
+    // int index = 0; 
+    // for(int i = 0; i < inputString.Length; i++) 
+    // { 
+    // if(inputString[i]=='-' && char.IsDigit(inputString[i + 1])) 
+    // { 
+    // Console.Write('-'); 
+    // index = inputString.IndexOf(inputString[i]); 
+    // break; 
+    // } 
+    // } 
+    // return index; 
+
+    //} 
+
+    //private static int Ibh(string inputString, int index) 
+    //{ 
+    // StringBuilder stringBuilder = new StringBuilder(); 
+    // for (int i = index; i < inputString.Length; i++) 
+    // { 
+    // if (char.IsDigit(inputString[i+1])) 
+    // { 
+    // stringBuilder.Append(inputString[i+1]); 
+    // if (inputString[i+1] == inputString[inputString.Length - 1] || char.IsDigit(inputString[i + 2])) 
+    // { 
+    // continue; 
+    // } 
+    // else break; 
+    // } 
+
+    // } 
+    // int result = int.Parse(stringBuilder.ToString()); 
+    // return result; 
+    //} 
+  }
+}
